c8e7848ab1fec4d5eac2dbccc20f537191a3ffe0
Shihan Chowdhury <hackedgdown@gmail.com>
2023-05-23 16:30:41 +0100

    74-7 Featured items section TODO

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 * 74-3 Create simple Top Banner react responsive carousel
 * 74-4 Use swiper JS to create a category Swiper
 * 74-5 Shared section title for different sections
+* 74-6 Show Popular menu items in the home page
+* 74-7 Featured items section
 
 
 ## react-boilerplate-2
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
 import PopularMenu from '../../Shared/PopularMenu/PopularMenu';
+import FeaturedSection from '../FeaturedSection/FeaturedSection';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Category/>
             <PopularMenu/>
+            <FeaturedSection/>
         </div>
     );
 };
